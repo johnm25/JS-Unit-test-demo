5e67f788314d00ae293d2bb97b90c9d3259ce827
johnm25 <johnm252001@yahoo.com>
2015-11-06 15:50:45 +0100

    update

---
+++ JSdemo/MVC.Typeahead/Service/CarService.svc.cs
@@ -0,0 +1,116 @@
+﻿namespace MVC.Typeahead.Service
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Activation;
+    using System.ServiceModel.Web;
+    using System.Text;
+    
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class CarService : ICarService
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+
+        public CarService()
+        {
+            
+        }
+
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<CarViewModel> GetCars()
+        {
+            return xx;
+        }
+
+        private static List<CarViewModel> xx = new List<CarViewModel>
+                                                   {
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "Audi",
+                                                               Model = "A4",
+                                                               Variant = "1.8 TDI Avant",
+                                                               Price = "412.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "Audi",
+                                                               Model = "A4",
+                                                               Variant = "2.0 TDI Avant",
+                                                               Price = "212.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "Audi",
+                                                               Model = "S4",
+                                                               Variant = "2.0 TDI Avant",
+                                                               Price = "212.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "Audi",
+                                                               Model = "S4",
+                                                               Variant = "2.0 TDI Avant",
+                                                               Price = "212.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "Audi",
+                                                               Model = "RS4",
+                                                               Variant = "2.0 TDI Avant",
+                                                               Price = "212.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "Audi",
+                                                               Model = "RS4",
+                                                               Variant = "2.0 TDI Avant",
+                                                               Price = "212.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "BMW",
+                                                               Model = "114d",
+                                                               Variant = "3d",
+                                                               Price = "212.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "BMW",
+                                                               Model = "116d",
+                                                               Variant = "d5",
+                                                               Price = "212.000"
+                                                           },
+                                                       new CarViewModel
+                                                           {
+                                                               Brand = "Citroën",
+                                                               Model = "C4",
+                                                               Variant = "Picasso 1,6 e-HDi 115 Intensive",
+                                                               Price = "212.000"
+                                                           },
+
+                                                   };
+    }
+
+    public interface ICarService
+    {
+        List<CarViewModel> GetCars();
+    }
+
+    public class CarViewModel
+    {
+        public string Variant { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ JSdemo/MVC.Typeahead/js/the-basics.js
@@ -0,0 +1,101 @@
+﻿$(function () {
+    "use strict";
+
+    var substringMatcher = function (strs) {
+        return function findMatches(q, cb) {
+            var matches, substringRegex;
+
+            // an array that will be populated with substring matches
+            matches = [];
+
+            // regex used to determine if a string contains the substring `q`
+            substrRegex = new RegExp(q, 'i');
+
+            // iterate through the pool of strings and for any string that
+            // contains the substring `q`, add it to the `matches` array
+            $.each(strs, function (i, str) {
+                if (substrRegex.test(str)) {
+                    matches.push(str);
+                }
+            });
+
+            cb(matches);
+        };
+    };
+
+    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+      'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+      'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+      'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+      'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+      'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+      'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+      'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+      'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+    ];
+
+
+    var houndStates = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        // `states` is an array of state names defined in "The Basics"
+        local: states
+    });
+
+
+    $('#the-basics .typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        name: 'states',
+        //display: 'item',
+        source: houndStates,
+        //source: substringMatcher(states)
+        templates: {
+            empty: [
+                '<div class="empty-message">',
+                'unable to find any match the current query',
+                '</div>'
+            ].join('\n'),
+            header: '<h3 class="league-name">Header</h3>',
+            footer: '<h3 class="league-name">Footer</h3>',
+            suggestion: Handlebars.compile('<div><strong>{{this}}</strong> – hello</div>')
+        }
+    });
+
+    Handlebars.registerHelper("debug", function (optionalValue) {
+        console.log("Current Context");
+        console.log("====================");
+        console.log(this);
+
+        if (optionalValue) {
+            console.log("Value");
+            console.log("====================");
+            console.log(optionalValue);
+        }
+    });
+
+
+
+    var xx = function CallService() {
+        $.ajax({
+            type: "GET", //GET or POST or PUT or DELETE verb
+            url: "Service/CarService.svc/GetCars", // Location of the service
+            data: "", //Data sent to server
+            contentType: "application/json; charset=utf-8", // content type sent to server
+            dataType: "json", //Expected data format from server
+            processdata: true, //True or False
+            success: function (result) {
+
+                debugger;
+            },
+            error: function ServiceFailed(result) {
+                alert('Service call failed: ' + result.status + '' + result.statusText);
+            }
+        });
+    }
+
+    xx();
+});
